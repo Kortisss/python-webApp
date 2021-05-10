f258a4dcb57aff62d4a0f0728d61e8e6f9b75e54
Michał Pieczka <michalpieczka99@gmail.com>
2021-05-10 19:12:54 +0200

    displaying uploaded images

--- website/views.py
+++ website/views.py
@@ -12,6 +12,7 @@ import base64
 views = Blueprint('views', __name__)
 Linkslist = []
 listOfResposnes = []
+listOfBase64Images = []
 
 @views.route('/', methods=['GET', 'POST'])
 @login_required
@@ -43,53 +44,67 @@ def delete_note():
 
 @views.route('/imgur', methods=['GET', 'POST'])
 def imgur():
-    
-    if request.method == "POST":
-        if request.files:
-            #getting filename form POST request
-            image = request.files["image"]
-            filename = image.filename
-            print(filename)
-
-            #searching for specific file in system
-            for root, dirs, files in os.walk(r'C:\Users'): 
-                for name in files:
-                    if name == filename:
-                        destination = os.path.abspath(os.path.join(root, name))
-
-            with open(destination, 'rb') as binary_file:
-                binary_file_data = binary_file.read()
-                base64_encoded_data = base64.b64encode(binary_file_data)
-                base64_message = base64_encoded_data.decode('utf-8')
-
-            url = "https://api.imgur.com/3/image"
+    """def createAlbum():
+        if request.method == "POST":
+            urlCreateAlbum = "https://api.imgur.com/3/album"
             payload={
-                'image': base64_message
+            'ids[]': 'x4ulVyk',
+            'title': request.form.get("title"),
+            'description': request.form.get("description"),
             }
+
             headers = {
-            'Authorization': 'Client-ID 4bf17ac773aa532',
-            'Cookie': 'frontpagebetav2=1; postpagebeta=1; pp=4520028185494238',
-            "type": "base64"
+            'Authorization': 'Client-ID 4bf17ac773aa532'
             }
 
-            response = requests.request("POST", url, headers=headers, data=payload)
-            
-            print(response.text)
+            response = requests.request("POST", urlCreateAlbum, headers=headers, data=payload)
             u = json.loads(response.text)
-            linkToFileImgur = (u["data"]["link"])
             if(response.status_code == 200):
-                flash(f"przesłano plik: {filename}; {linkToFileImgur}", category='success')
-                Linkslist.append(linkToFileImgur)
-                listOfResposnes.append(response.json())
-            
-            
+                flash(f"Stworzono album: {u['data']['id']}", category='success')
+                print(response.text)"""
+    def uploadImage():
+        if request.method == "POST":
+            if request.files:
+                #getting filename form POST request
+                image = request.files["image"]
+                filename = image.filename
+                print(filename)
+
+                #searching for specific file in system
+                for root, dirs, files in os.walk(r'C:\Users'): 
+                    for name in files:
+                        if name == filename:
+                            destination = os.path.abspath(os.path.join(root, name))
+
+                with open(destination, 'rb') as binary_file:
+                    binary_file_data = binary_file.read()
+                    base64_encoded_data = base64.b64encode(binary_file_data)
+                    base64_message = base64_encoded_data.decode('utf-8')
+
+                    listOfBase64Images.append(base64_message)
+
+                urlUploadImage = "https://api.imgur.com/3/image"
+                payload={
+                    'image': base64_message
+                }
+                headers = {
+                'Authorization': 'Client-ID 4bf17ac773aa532',
+                "type": "base64"
+                }
+                response = requests.request("POST", urlUploadImage, headers=headers, data=payload)
+                print(response.text)
+
+                u = json.loads(response.text)
+                linkToFileImgur = (u["data"]["link"])
+                if(response.status_code == 200):
+                    flash(f"przesłano plik: {filename}; {linkToFileImgur}", category='success')
+                    Linkslist.append(linkToFileImgur)
+                    listOfResposnes.append(response.json())  
+
+    def getUploadedImages():
+        pass
+    
+    uploadImage()          
     print(Linkslist)
-    return render_template("imgur.html", user=current_user, myNewList = Linkslist)
-
-
-
-@views.route("/upload-image", methods=["GET", "POST"])
-def uploadImage():
-
-    return render_template('upload-image.html', user=current_user)
+    return render_template("imgur.html", user=current_user, listOfBase64Images = listOfBase64Images, Linkslist = Linkslist)
 
